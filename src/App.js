d06f9cdd12892b836724da684526a49b2de24ca9
krasapchik <kutman_mamytov359@gmail.com>
2023-04-24 14:11:36 +0600

    React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Conculete from './pages/Conculete';
+
+function App() {
+  return (
+    <div className="App">
+     <Conculete/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Conculete.js
@@ -0,0 +1,59 @@
+
+import React from 'react'
+import { useState } from 'react'
+const Conculete = () => {
+  const [one, setOne] = useState("")
+const [two, setTwo] = useState("")
+  
+  return (
+    <div>
+      <input onChange={(e) => {
+      setOne(e.target.value)
+    }} type="number" />
+    <input type="number" onChange={(e) => {
+      setTwo(e.target.value)
+    }} />
+    <button onClick={() => {
+
+      if (one == 0 || two == 0) {
+        alert("заполните поля")
+      }
+      else {
+
+        alert(+one + +two)
+      }
+    }}>плюс</button>
+    <button onClick={() => {
+
+      if (one == 0 || two == 0) {
+        alert("заполните поля")
+      }
+      else {
+
+        alert(+one - +two)
+      }
+    }}>минус</button>
+    <button onClick={() => {
+
+      if (one == 0 || two == 0) {
+        alert("заполните поля")
+      }
+      else {
+
+        alert(+one * +two)
+      }
+    }}>умножить</button>
+    <button onClick={() => {
+
+      if (one == 0 || two == 0) {
+        alert("заполните поля")
+      }
+      else {
+
+        alert(+one / +two)
+      }
+    }}>разделить</button></div>
+  )
+}
+
+export default Conculete
